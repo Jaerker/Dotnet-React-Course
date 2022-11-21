887e65235e357c00ae5c6dc26e2bcd3643398cab
Jaerker <johanjakberger@gmail.com>
2022-11-21 12:50:04 +0100

    Initialize git

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,28 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace API.Controllers;
+
+public class ActivitiesController : BaseApiController
+{
+    private readonly DataContext context;
+
+    public ActivitiesController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities(){
+
+        return await context.Activities.ToListAsync();
+        
+    }
+
+    [HttpGet("{id}")] // activities/id
+    public async Task<ActionResult<Activity>> GetActivity(Guid id){
+        return await context.Activities.FindAsync(id);
+    }
+}
